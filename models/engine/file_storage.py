0a4e7a268228f4b13d0dcbcf540669cb2fda8cf6
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-09 18:17:44 +0100

    updated fileStorage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,16 +18,21 @@ class Filestorage:
         __objects: a dict to store the objects
     """
 
-    __file_path = 'file.json'
-    __objects = {}
+    def __init__(self):
+        """ initializes the class attributes """
+        self.__file_path = 'file.json'
+        self.__objects = {}
 
     def all(self):
         """ returns all the objects """
         return self.__objects
 
+    def commit(self, obj):
+        """ stages an object for saving it to a file """
+        name = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[name] = obj
+
     def save(self, obj):
         """ saves the instance of an object to a file """
-        with open(self.__file_path, "w+") as f:
-            f.write()
-
-
+        with open(self.__file_path, "w") as f:
+            json.dump(self.__object, f)
