eb4a5cc368a8d33c9c085ad96153b248c8642e9a
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-09 11:01:00 +0100

    updated file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 AirBnB clone - The console
-File_storage
+File_storage module
 """
 
 import json
@@ -10,11 +10,24 @@ from models.base_model import BaseModel
 
 class Filestorage:
     """
-    class sterializes instances to a JSON file
-    and deserializes JSON file to instances
+    
+    FileStorage - sterializes instances to a JSON file
+                and deserializes JSON file to instances
+    Args:
+        __file_path: the path to store the objects
+        __objects: a dict to store the objects
     """
 
     __file_path = 'file.json'
     __objects = {}
+
+    def all(self):
+        """ returns all the objects """
+        return self.__objects
+
+    def save(self, obj):
+        """ saves the instance of an object to a file """
+        with open(self.__file_path, "w+") as f:
+            f.write()
 
 
